0d9b1a85a7daea6775f228d2e97649262aed2e00
Paweł Dziergas <pawel.dziergas@paxillus.pl>
2025-05-16 17:53:11 +0200

    Add project files.

---
+++ RekurencjaICiekaweFunkcje/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using RekurencjaICiekaweFunkcje;
+
+Console.WriteLine("Hello, World!");
+
+SolvedTasks solvedTasks = new();
+
+//Rekurencja - Ciąg fibonacciego
+/* Zad 1. Napisz funkcję liczącą kolejne liczby ciągu fibonacciego z wykorzystaniem rekurencji.
+ * podpowiedź: ciąg fibonacciego to taka sekwencja:
+ * 1 1 2 3 5 8 13 21 ...
+ * każdy element ciągu fib można zapisać jako sumę dwóch poprzednich liczb:
+ * 
+ * n = n-1 + n-2
+ */
+
+solvedTasks.DisplayNthFibonacciNumber(10);
+
+//Rekurencja - Inny przykład ciągu
+/* Zad 2. Za pomocą rekurencji policz kolejne elementy ciągu opisanego daną funkcją:
+ * 
+ * an =         2   dla n = 1
+ *      {  an-1 * 2 dla n nieparzystego
+ *         an-1 + 2 dla n parzystego
+ */
+
+solvedTasks.DisplayCustomSeries(10);
+//Powtórka z poprzednich zajęć tablice i proste algorytmy
+/* Zad 3. Zadeklaruj tablicę 10 elementową i wypełnij ją liczbami (nie korzystaj z petli od 0 do size wpisz ręcznie numerki)
+ * Następnie napisz 2 osobne funkcje: min i max, które kolejno zwracają minimalny element w tablicy i maksymalny element w tablicy.
+ * funkcje zwracają int.
+ * Na koniec wypisz wszystkie elementy tablicy, najmiejszy i największy element tablicy.
+ */
+int[] arr = new int[10] { 1, -8, 2, 7, -6, 5, 16, 4, -11, 18 };
+solvedTasks.MinMaxArray(arr);
---
+++ RekurencjaICiekaweFunkcje/SolvedTasks.cs
@@ -0,0 +1,87 @@
+﻿using System.ComponentModel.Design;
+
+namespace RekurencjaICiekaweFunkcje;
+
+public class SolvedTasks
+{
+    //Zad 1
+    private static int Fib(int n)
+    {
+        if (n == 1 || n == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+
+    public void DisplayNthFibonacciNumber(int n)
+    {
+        string result = "";
+        for (int i = 0; i < n; i++)
+        {
+            result += Fib(i).ToString() + " ";
+        }
+        result += "\n";
+        Console.WriteLine(result);
+    }
+
+    //Zad 2
+    private static int CustomSeries(int n)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+        else if (n == 1)
+        {
+            return 2;
+        }
+        else if (n % 2 != 0)
+        {
+            return CustomSeries(n - 1) * 2;
+        }
+        else
+        {
+            return CustomSeries(n - 1) + 2;
+        }
+    }
+    public void DisplayCustomSeries(int n)
+    {
+        string result = "";
+        for (int i = 0; i < n; i++)
+        {
+            result += CustomSeries(i).ToString() + " ";
+        }
+        result += "\n";
+        Console.WriteLine(result);
+    }
+
+    //Zad 3
+    public void MinMaxArray(int[] arr)
+    {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (min > arr[i])
+            {
+                min = arr[i];
+            }
+            if (max < arr[i])
+            {
+                max = arr[i];
+            }
+        }
+        string result = "";
+        for (int i = 0; i < arr.Length; i++)
+        {
+            result += arr[i] + " ";
+        }
+        Console.WriteLine(result);
+        Console.WriteLine("Najmniejsza wartość w tablicy: " + min);
+        Console.WriteLine("Największa wartość w tablicy: " + max);
+    }
+}
